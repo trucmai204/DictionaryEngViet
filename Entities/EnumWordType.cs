e91e257f03dc963bfefe29cd5090b163f67f5a75
Truc Mai cua Quang <mtruc7097@gmail.com>
2024-07-14 22:20:55 +0700

    add entity

---
+++ Entities/EnumWordType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public enum EnumWordType
+    {
+        Noun = 1,
+        Adjective = 2,
+        Verb = 3,
+        Adverb = 4
+    } 
+}
---
+++ Entities/Vocabulary.cs
@@ -0,0 +1,12 @@
+﻿namespace Entities
+{
+    public class Vocabulary
+    {
+        public int Id { get; set; }
+        public string Word { get; set; }
+        public EnumWordType WordTypeId { get; set; }
+        public string Pronounciation { get; set; }
+        public string Description { get; set; }
+
+    }
+}
